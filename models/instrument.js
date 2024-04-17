05a30f568cd1507ae781c59192b18585510278e1
Sai Achyuth Konda <saiachyuthkonda@gmail.com>
2024-04-17 15:34:32 -0500

    all commits done

--- models/instrument.js
+++ models/instrument.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const instrumentSchema = mongoose.Schema({
 instrument_type: String,
-instrument_size: String,
-instrument_price: Number
+instrument_size: { type: String, minlength: 1, maxlength:25 },
+instrument_price: { type: Number, min: 0, max: 25000 }
 })
 module.exports = mongoose.model("instrument",
 instrumentSchema)
--- routes/instrument.js
+++ routes/instrument.js
@@ -1,6 +1,17 @@
 var express = require('express');
 const instrument_controlers= require('../controllers/instrument');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on
+
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ res.redirect("/login");
+ }
+
 /* GET instrument */
 router.get('/',instrument_controlers.instrument_view_all_Page );
  
@@ -8,11 +19,11 @@ router.get('/',instrument_controlers.instrument_view_all_Page );
 router.get('/detail', instrument_controlers.instrument_view_one_Page);
  
 /* GET create instrument page */
-router.get('/create', instrument_controlers.instrument_create_Page);
+router.get('/create', secured, instrument_controlers.instrument_create_Page);
  
-router.get('/update', instrument_controlers.instrument_update_Page);
+router.get('/update', secured, instrument_controlers.instrument_update_Page);
 
 /* GET delete instrument page */
-router.get('/delete', instrument_controlers.instrument_delete_Page);
+router.get('/delete', secured, instrument_controlers.instrument_delete_Page);
 
 module.exports = router;
